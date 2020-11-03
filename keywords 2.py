b4dbb5f5a4745a5cc3e3ca6352f71d41b7157dd3
mokkksh <73828893+mokkksh@users.noreply.github.com>
2020-11-03 17:29:36 +0530

    Add files via upload

---
+++ keywords 2.py
@@ -0,0 +1,11 @@
+# and, open, not
+print(True and False)
+print(True or False)
+print(not False)
+print(True and True)
+print(True or True)
+print(not True)
+
+# as
+import math as MyMath
+print(MyMath.cos(MyMath.pi))
